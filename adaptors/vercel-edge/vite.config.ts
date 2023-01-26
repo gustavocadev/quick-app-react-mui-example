e75b1ab1147ec58acaad0935767a8d4aad890379
gustavocadev <churaalaygustavo11@gmail.com>
2023-01-26 15:53:12 -0500

    feat:  delete edge

--- adaptors/vercel-edge/vite.config.ts
+++ adaptors/vercel-edge/vite.config.ts
@@ -1,4 +1,3 @@
-import { vercelEdgeAdaptor } from '@builder.io/qwik-city/adaptors/vercel-edge/vite';
 import { extendConfig } from '@builder.io/qwik-city/vite';
 import baseConfig from '../../vite.config';
 
@@ -6,15 +5,7 @@ export default extendConfig(baseConfig, () => {
   return {
     build: {
       ssr: true,
-      rollupOptions: {
-        input: ['src/entry.vercel-edge.tsx', '@qwik-city-plan'],
-      },
-      outDir: '.vercel/output/functions/_qwik-city.func',
     },
-    plugins: [
-      vercelEdgeAdaptor({
-        staticGenerate: true,
-      }),
-    ],
+    plugins: [],
   };
 });
